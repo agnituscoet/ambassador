f98f514cb5bc2bd0c571f045841607f1751fc85e
Karthik N P <79256465+karthik-n-p@users.noreply.github.com>
2023-04-08 00:55:37 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,25 +1,66 @@
 <?php
-// Get data from form
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form input values
+    $name = sanitizeInput($_POST["name"]);
+    $college = sanitizeInput($_POST["college"]);
+    $year = sanitizeInput($_POST["year"]);
+    $mobile = sanitizeInput($_POST["mobile"]);
+    $email = sanitizeInput($_POST["email"]);
+    $password = sanitizeInput($_POST["password"]);
+    $text = sanitizeInput($_POST["text"]);
 
-$to = "agnituscoet@gmail.com";
-$subject = "This is the subject line";
+    // Set "from" email as the inputted email
+    $from = $email;
 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
-$txt ="Name = ". $name . "\r\n Email = "
-	. $email . "\r\n Message =" . $message;
+    // Recipient email (change this to your desired email address)
+    $to = "agnituscoet@gmail.com,ayanap.27122002@gmail.com";
 
-$headers = "From: agnituscoet@gmail.com" . "\r\n" .
-			"CC: agnituscoet@gmail.com";
-if($email != NULL) {
-	mail($to, $subject, $txt, $headers);
+    // Subject of the email
+    $subject = "Agnitus Ambassador Program Sign Up";
+
+    // Message body
+    $message = "Name: " . $name . "\n"
+        . "College: " . $college . "\n"
+        . "Year of Study: " . $year . "\n"
+        . "Mobile: " . $mobile . "\n"
+        . "Email: " . $email . "\n"
+        . "Password: " . $password . "\n"
+        . "Queries: " . $text;
+
+    // Additional headers
+    $headers = "From:" . $from . "\r\n";
+    $headers .= "Reply-To:" . $email . "\r\n"; // Added Reply-To header for better email handling
+
+    // Additional security headers
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+
+    // Send the email
+    $mailSent = mail($to, $subject, $message, $headers);
+
+    // Check if email was sent successfully
+    if ($mailSent) {
+        // Redirect to success page
+        header("Location: success.html");
+        exit();
+    } else {
+        // Redirect to error page
+        header("Location: error.html");
+        exit();
+    }
 }
 
-// Redirect to
-header("Location:last.html");
+// Function to sanitize input data
+function sanitizeInput($input)
+{
+    // Remove leading/trailing white spaces
+    $input = trim($input);
+    // Remove backslashes
+    $input = stripslashes($input);
+    // Convert special characters to HTML entities
+    $input = htmlspecialchars($input);
+    return $input;
+}
 ?>
